f076c0008741607360e4fd94bbec798761332cdf
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-03-22 00:40:35 -0400

    got double tap gesture to dismiss popped up pictures

--- pixel-city/Controller/PopVC.swift
+++ pixel-city/Controller/PopVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PopVC: UIViewController {
+class PopVC: UIViewController, UIGestureRecognizerDelegate {
     
     // OUTLETS
     @IBOutlet weak var popImageView: UIImageView!
@@ -23,5 +23,17 @@ class PopVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         popImageView.image = passedImage
+        addDoubleTap()
+    }
+    
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func screenWasDoubleTapped() {
+        dismiss(animated: true, completion: nil)
     }
 }
